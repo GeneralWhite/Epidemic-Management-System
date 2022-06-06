bf5a4b1d56fc9027baa60d829cb50aac5e91e076
Jerrold <5445687d@gmail.com>
2022-06-06 19:09:13 +0800

    map

--- src/Regional_Situation.java
+++ src/Regional_Situation.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Regional_Situation
 {
@@ -9,10 +10,18 @@ public class Regional_Situation
         JFrame frame = new JFrame("Regional Situation");
         frame.setContentPane(new Regional_Situation().root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1200, 800);
+        frame.setLayout(null);
+        frame.setSize(1000, 800);
         // 设置窗口相对于指定组件的位置
         // 如果参数为 null，则此窗口将置于屏幕的中央。
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        JLabel label1 = new JLabel();
+        label1.setIcon(new ImageIcon("src/4.png"));
+        frame.add(label1);
+        label1.setBounds(50, 50, 481, 350);
+
+        frame.setResizable(false);
     }
 }
